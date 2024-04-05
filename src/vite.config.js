915579a612aca648ad7f68d35d61300d9764627e
Will Rynearson <will@developmentseed.org>
2024-04-05 13:52:11 +0200

    update again

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
 
 // from https://vitejs.dev/config/#using-environment-variables-in-config
 export default defineConfig({
   // Other Vite config...
-  plugins: [react()],
   base: "/us-census-language/",
 });
